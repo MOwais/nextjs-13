b431797ba82c21e4c52c172d7f3f5074668e7804
MOwais <owace.ahmed@gmail.com>
2023-09-29 20:43:16 -0700

    routes and nested routes

---
+++ next_13_crash/app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Owais Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ullam
+        eum rem dicta aliquam? Vero placeat doloremque nostrum dolorum laborum
+        beatae, porro incidunt quibusdam id enim consequuntur ipsa obcaecati,
+        asperiores inventore facilis aliquam quod. Enim a in sint exercitationem
+        debitis itaque qui beatae soluta excepturi, saepe maiores minus totam
+        corrupti.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ next_13_crash/app/about/team/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quisquam
+        atque iusto? Rem suscipit dolores laboriosam tempora obcaecati molestias
+        et, perspiciatis sint? Tempore voluptas magnam quasi nemo temporibus
+        atque! Non.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;

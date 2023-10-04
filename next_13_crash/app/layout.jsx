a0700785b98f092849a2491b7f6953aafcc8d934
MOwais <owace.ahmed@gmail.com>
2023-10-03 17:55:03 -0700

    change inter font to Poppins

--- next_13_crash/app/layout.jsx
+++ next_13_crash/app/layout.jsx
@@ -1,7 +1,10 @@
+import { Poppins } from "next/font/google";
 import "./globals.css";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Owais Media",
@@ -13,7 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <main className="container">{children}</main>
+      </body>
     </html>
   );
 }

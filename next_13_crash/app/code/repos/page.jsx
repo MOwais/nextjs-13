6b517b4ad8cb897977e3a6b3328f83c6e36c0b58
MOwais <owace.ahmed@gmail.com>
2023-10-03 18:30:04 -0700

    header component, repos page

---
+++ next_13_crash/app/code/repos/page.jsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
+
+async function fetchRepos() {
+  const response = await fetch("https://api.github.com/users/mowais/repos");
+  const repos = await response.json();
+  return repos;
+}
+
+const ReposPage = async () => {
+  const repos = await fetchRepos();
+
+  return (
+    <div className="repos-container">
+      {<h1>Repositories</h1>}
+      <ul className="repo-list">
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <d className="repo-details">
+                <span>
+                  <FaStar /> {repo.stargazers_count}
+                </span>
+                <span>
+                  <FaCodeBranch /> {repo.forks_count}
+                </span>
+                <span>
+                  <FaEye /> {repo.watchers_count}
+                </span>
+              </d>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ReposPage;

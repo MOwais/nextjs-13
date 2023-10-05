fbe460d96ead363ac9eb5b05281f208b0b9f235f
MOwais <owace.ahmed@gmail.com>
2023-10-04 21:46:30 -0700

    course search

--- next_13_crash/app/page.jsx
+++ next_13_crash/app/page.jsx
@@ -1,12 +1,31 @@
-import Link from "next/link";
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
 import Courses from "./components/Courses";
+import LoadingPage from "./loading";
+import CourseSearch from "./components/CourseSearch";
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch("/api/courses");
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) return <LoadingPage />;
+
   return (
     <div>
       <h1>Welcome to Owais Media</h1>
-      <Courses />
+      <CourseSearch getSearchResults={(results) => setCourses(results)} />
+      <Courses courses={courses} />
     </div>
   );
 };

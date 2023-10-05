37aac4540dd0c4b56db0a527c5b97a7851163634
MOwais <owace.ahmed@gmail.com>
2023-10-04 21:13:46 -0700

    api routes to get courses json

--- next_13_crash/app/page.jsx
+++ next_13_crash/app/page.jsx
@@ -1,21 +1,12 @@
 import Link from "next/link";
 import React from "react";
+import Courses from "./components/Courses";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to Owais Media</h1>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/about/team">Team</Link>
-        </li>
-      </ul>
+      <Courses />
     </div>
   );
 };
